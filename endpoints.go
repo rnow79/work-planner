6b680c41467cffa401bda5040d87b53f1b3279cf
Arnau Carrasco <arnau.carrasco@gmail.com>
2022-01-23 12:28:02 +0100

    endpoints formatting

--- endpoints.go
+++ endpoints.go
@@ -11,12 +11,12 @@ import (
 )
 
 // getShifts Endpoint: admins can get all shifts of the week, or a specific user's
-// shifts. Workers can just list their own endpoints
+// shifts. Workers can just list their own endpoints.
 func getShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 	if isAdmin(r) {
 		// Admin GET Method
 		if userid := r.URL.Query().Get("userid"); len(userid) == 0 {
-			// Without params, send all working plan
+			// Without params, send all working plan.
 			sendJSON(w, workingPlan)
 			return
 		} else {
@@ -25,11 +25,11 @@ func getShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 				sendBadRequest(w, "userid must be numeric")
 				return
 			}
-			// Send specific user shifts
+			// Send specific user shifts.
 			sendJSON(w, workingPlan.GetUserShifts(userid))
 		}
 	} else {
-		// Worker GET Method
+		// Worker GET Method.
 		sendJSON(w, workingPlan.GetUserShifts(getUserIdFromHeader(r)))
 		return
 	}
@@ -39,12 +39,13 @@ func getShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 func postShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if isAdmin(r) {
-		// Admin POST Method (Error 400)
+		// Admin POST Method (Error 400).
 		sendBadRequest(w, "admins cannot request shifts")
 		return
 	} else {
-		// Worker POST Method
+		// Worker POST Method.
 		if day, shift := r.PostForm.Get("day"), r.PostForm.Get("shift"); len(day) == 0 || len(shift) == 0 {
+			// Missing parameters.
 			sendBadRequest(w, "missing day or shift")
 			return
 		} else {
@@ -58,26 +59,28 @@ func postShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 				sendBadRequest(w, "shift must be numeric")
 				return
 			}
-			// Try to insert shift
+			// Try to insert shift.
 			err = workingPlan.InsertUserShift(getUserIdFromHeader(r), day, shift)
 			if err != nil {
 				sendBadRequest(w, err.Error())
 				return
 			}
-			// If not already present, insert user
+			// If not already present, insert user.
 			if !workingPlan.HasUser(getUserIdFromHeader(r)) {
 				workingPlan.InsertUser(r.Header.Get("X-User"), r.Header.Get("X-Name"), getUserLevelFromHeader(r), getUserIdFromHeader(r))
 			}
-			// Send current user shifts
+			// Send current user shifts.
 			sendJSON(w, workingPlan.GetUserShifts(getUserIdFromHeader(r)))
 			return
 		}
 	}
 }
 
+// deleteShiftsEndpoint: workers can delete their own shifts. Admins can delete any shift.
 func deleteShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
-	// Worker DELETE Method
+	// Worker DELETE Method.
 	if day, shift := r.URL.Query().Get("day"), r.URL.Query().Get("shift"); len(day) == 0 || len(shift) == 0 {
+		// Missing parameters.
 		sendBadRequest(w, "missing day or shift")
 		return
 	} else {
@@ -91,7 +94,7 @@ func deleteShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 			sendBadRequest(w, "shift must be numeric")
 			return
 		}
-		// Try to delete user shift
+		// Try to delete user shift.
 		err = workingPlan.DeleteUserShift(getUserIdFromHeader(r), day, shift)
 		if err != nil {
 			sendBadRequest(w, err.Error())
@@ -100,10 +103,11 @@ func deleteShiftsEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Anonymous function for creating tokens with the current server signing key
-func getTokenEndpoint(w http.ResponseWriter, r *http.Request) {
+// Anonymous function for creating tokens with the current server signing key.
+func postTokenEndpoint(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if usr, nam, lvl, uid := r.PostForm.Get("usr"), r.PostForm.Get("nam"), r.PostForm.Get("lvl"), r.PostForm.Get("uid"); len(usr) == 0 || len(nam) == 0 || len(lvl) == 0 || len(uid) == 0 {
+		// Missing parameters.
 		fmt.Fprintf(w, "error: not enough parameters")
 		return
 	} else {
@@ -134,43 +138,45 @@ func getTokenEndpoint(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// ---------------------------------
 // Other functions used by endpoints
+// ---------------------------------
 
-// Log a line if the program is running in verbose mode
+// Log a line if the program is running in verbose mode.
 func logLine(format string, v ...interface{}) {
 	if verbose {
 		log.Println(fmt.Sprintf(format, v...))
 	}
 }
 
-// Send errors as 400 - Bad Request
+// Send errors as 400 - Bad Request.
 func sendBadRequest(w http.ResponseWriter, err string) {
 	http.Error(w, err, http.StatusBadRequest)
 }
 
-// Send forbidden
+// Send forbidden.
 func sendForbidden(w http.ResponseWriter) {
 	http.Error(w, "Forbidden", http.StatusForbidden)
 }
 
-// Serialize objects in JSON and send
+// Serialize objects in JSON and send.
 func sendJSON(w http.ResponseWriter, obj interface{}) {
 	w.Header().Set("Content-Type", "text/json; charset=utf-8")
 	json.NewEncoder(w).Encode(obj)
 }
 
-// Determine if user is an admin
+// Determine if user is an admin.
 func isAdmin(r *http.Request) bool {
 	return r.Header.Get("X-Level") == "1"
 }
 
-// Extract userid from header and return as an integer
+// Extract userid from header and return as an integer.
 func getUserIdFromHeader(r *http.Request) int {
 	i, _ := strconv.Atoi(r.Header.Get("X-Userid"))
 	return i
 }
 
-// Extract level from header and return as an integer
+// Extract level from header and return as an integer.
 func getUserLevelFromHeader(r *http.Request) int {
 	i, _ := strconv.Atoi(r.Header.Get("X-Level"))
 	return i
